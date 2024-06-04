148301c3be850ee0e99f8011d890e38c6347d5c6
kaane5662 <kaane0169@gmail.com>
2024-06-04 18:36:23 -0400

    added exception to vstore

--- db.py
+++ db.py
@@ -10,12 +10,17 @@ load_dotenv()
 ASTRA_DB_APPLICATION_TOKEN = os.environ.get("ASTRA_DB_APPLICATION_TOKEN")
 ASTRA_DB_API_ENDPOINT = os.environ.get("ASTRA_DB_API_ENDPOINT")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+try:
 
-vstore = AstraDBVectorStore(
-    embedding=OpenAIEmbeddings(api_key=OPENAI_API_KEY),
-    collection_name="articlestore1",
-    token=ASTRA_DB_APPLICATION_TOKEN,
-    api_endpoint=ASTRA_DB_API_ENDPOINT,
-    namespace="research_papers"
-)
+    vstore = AstraDBVectorStore(
+        embedding=OpenAIEmbeddings(api_key=OPENAI_API_KEY),
+        collection_name="articlestore1",
+        token=ASTRA_DB_APPLICATION_TOKEN,
+        api_endpoint=ASTRA_DB_API_ENDPOINT,
+        namespace="research_papers"
+    )
+
+except Exception as e:
+    print(f"Failed to connect ASTRA DB Vector store: {e}")
+
 
